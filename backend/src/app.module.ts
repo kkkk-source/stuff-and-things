a1ce9bbb3303dc5ee0dea7d2bdd77f42e9140c32
molly <mol.tired@gmail.com>
2021-07-24 04:08:49 -0500

    generate resource movements

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StuffsModule } from './stuffs/stuffs.module';
+import { MovementsModule } from './movements/movements.module';
 
 @Module({
-  imports: [StuffsModule],
+  imports: [StuffsModule, MovementsModule],
   controllers: [AppController],
   providers: [AppService],
 })
