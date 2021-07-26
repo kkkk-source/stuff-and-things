32987e866866d37461f798ae7ae0fddfe073beba
molly <mol.tired@gmail.com>
2021-07-26 00:03:28 -0500

    add Dockerfiles

--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://192.168.1.1:3000/api/v1',
+  baseURL: 'http://localhost:3000/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
